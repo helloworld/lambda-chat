e7070dbc3ba147d76a70d131c851419826012211
Josh Lee <jclee337@tuta.io>
2017-12-03 18:12:03 -0500

    Got dynamic route variable to be passed in as parameter

--- src/Handler/ChannelView.hs
+++ src/Handler/ChannelView.hs
@@ -3,12 +3,13 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE ViewPatterns #-}
 module Handler.ChannelView where
 
 import Import
 
-getChannelViewR :: Handler Html
-getChannelViewR = do
+getChannelViewR :: String -> Handler Html
+getChannelViewR slug = do
     (_, user) <- requireAuthPair
     defaultLayout $ do
         setTitle . toHtml $ userIdent user <> "'s User page"
